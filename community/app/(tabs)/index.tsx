77629459884b3baf0a32dfe978aa5b4e67f09c85
dahyeon777 <daaaa0727@gmail.com>
2025-04-17 22:15:14 +0900

    flexBox

--- community/app/(tabs)/index.tsx
+++ community/app/(tabs)/index.tsx
@@ -1,5 +1,44 @@
-import { View } from "react-native";
+import { useState } from "react";
+import { Text, View, SafeAreaView, StyleSheet, TextInput } from "react-native";
 
 export default function HomeScreen() {
-  return <View></View>;
+  return (
+    <SafeAreaView>
+      <View style={styles.parent}>
+        <View style={styles.container}>
+          <Text style={styles.text}>텍스트</Text>
+        </View>
+        <View style={styles.container2}>
+          <Text style={styles.text}>텍스트</Text>
+          <Text style={styles.text}>텍스트</Text>
+          <Text style={styles.text}>텍스트</Text>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
 }
+
+const styles = StyleSheet.create({
+  parent: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  container: {
+    backgroundColor: "yellow",
+    color: "",
+    // flex: 1,
+  },
+  container2: {
+    backgroundColor: "blue",
+    color: "",
+    // flex: 1,
+  },
+  text: {
+    color: "red",
+    fontSize: 30,
+  },
+  input: {
+    fontSize: 30,
+  },
+});
