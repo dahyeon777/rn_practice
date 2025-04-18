2cb10c176bc3786278c3b7f6697113c6f2f90465
dahyeon777 <daaaa0727@gmail.com>
2025-04-18 23:33:38 +0900

    navigation1

--- community/app/(tabs)/index.tsx
+++ community/app/(tabs)/index.tsx
@@ -1,44 +1,9 @@
-import { useState } from "react";
-import { Text, View, SafeAreaView, StyleSheet, TextInput } from "react-native";
+import { Text, SafeAreaView } from "react-native";
 
 export default function HomeScreen() {
   return (
     <SafeAreaView>
-      <View style={styles.parent}>
-        <View style={styles.container}>
-          <Text style={styles.text}>텍스트</Text>
-        </View>
-        <View style={styles.container2}>
-          <Text style={styles.text}>텍스트</Text>
-          <Text style={styles.text}>텍스트</Text>
-          <Text style={styles.text}>텍스트</Text>
-        </View>
-      </View>
+      <Text>홈스크린</Text>
     </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  parent: {
-    flexDirection: "row",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  container: {
-    backgroundColor: "yellow",
-    color: "",
-    // flex: 1,
-  },
-  container2: {
-    backgroundColor: "blue",
-    color: "",
-    // flex: 1,
-  },
-  text: {
-    color: "red",
-    fontSize: 30,
-  },
-  input: {
-    fontSize: 30,
-  },
-});
---
+++ community/app/(tabs)/my/index.tsx
@@ -0,0 +1,12 @@
+import {
+  Text,
+  SafeAreaView,
+} from "react-native";
+
+export default function MyScreen() {
+  return (
+    <SafeAreaView>
+        <Text>내정보 스크린</Text>
+    </SafeAreaView>
+  );
+}
